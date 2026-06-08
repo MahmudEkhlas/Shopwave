8d39ab2435735f716d7b0eba1258f3a0ffaedebd
Mahmud <mahmudekhlas70@gmail.com>
2026-06-09 00:38:23 +0530

    use product.js file

--- script/amazon.js
+++ script/amazon.js
@@ -1,41 +1,12 @@
-const product = [
-    {
-        img: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-        name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-        rating: {
-            stars: 4.5,
-            count: 87
-        },
-        price: 1090 //in cents
-    },
-    {
-        img: 'images/products/intermediate-composite-basketball.jpg',
-        name: 'Intermediate Size Basketball',
-        rating: {
-            stars: 4,
-            count: 127
-        },
-        price: 2095
-    },
-    {
-        img: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-        name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-        rating: {
-            stars: 4.5,
-            count: 56
-        },
-        price: 799
-    }
-];
 
-let prodhtml='';
+let prodhtml = '';
 
 product.forEach((prod) => {
-    prodhtml  += `
+  prodhtml += `
     <div class="product-container">
           <div class="product-image-container">
             <img class="product-image"
-              src="${prod.img}">
+              src="${prod.image}">
           </div>
 
           <div class="product-name limit-text-to-2-lines">
@@ -83,4 +54,4 @@ product.forEach((prod) => {
     `;
 });
 
-document.querySelector('.js-products-grid').innerHTML= prodhtml;
+document.querySelector('.js-products-grid').innerHTML = prodhtml;
