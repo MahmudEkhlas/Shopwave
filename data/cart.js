8d6cfd23f2d05b1099841abc3a89ba9ab0b3db12
Mahmud <mahmudekhlas70@gmail.com>
2026-06-12 11:32:47 +0530

    move add to cart to cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,25 @@
-export const cart =[];
+export const cart = [];
+
+//adding to the cart
+
+// function for adding the item to the cart
+export function add_to_cart(pro_id) {
+    let matching;
+
+    cart.forEach((item) => {
+        if (pro_id === item.productname) {
+            matching = item;
+        }
+    });
+
+    if (matching) {
+        matching.quantity++;
+    }
+
+    else {
+        cart.push({
+            productid: pro_id,
+            quantity: 1
+        });
+    }
+}
--- script/amazon.js
+++ script/amazon.js
@@ -1,5 +1,5 @@
-import { cart } from "../data/cart.js";
-import {product} from "../data/products.js"
+import { cart, add_to_cart} from "../data/cart.js";
+import { product } from "../data/products.js"
 let prodhtml = '';
 
 product.forEach((prod) => {
@@ -57,34 +57,20 @@ product.forEach((prod) => {
 
 document.querySelector('.js-products-grid').innerHTML = prodhtml;
 
-//adding to the cart
+//function for the updating total cart quantity in the front page
+function update_cart(){
+  let total_quantity = 0;
+
+    cart.forEach((item) => {
+      total_quantity += item.quantity;
+    });
+    document.querySelector('.js-cart-quantity').innerHTML = total_quantity;
+}
+
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
   button.addEventListener('click', () => {
     const pro_id = button.dataset.product_id;
-
-    let matching;
-
-    cart.forEach((item) => {
-      if (pro_id === item.productname) {
-        matching = item;
-      }
-    });
-
-    if (matching) {
-      matching.quantity++;
-    }
-
-    else {
-      cart.push({
-        productid: pro_id,
-        quantity: 1
-      });
-    }
-    let total_quantity=0;
-
-    cart.forEach((item) =>{
-      total_quantity += item.quantity;
-    });
-    document.querySelector('.js-cart-quantity').innerHTML=total_quantity;
+    add_to_cart(pro_id);
+    update_cart();
   });
 });
