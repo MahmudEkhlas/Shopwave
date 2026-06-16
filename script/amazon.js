73f506de3adb83e70cd50a0e608319040fe0a18d
Mahmud <mahmudekhlas70@gmail.com>
2026-06-16 12:45:23 +0530

    added is shown in the homepage

--- script/amazon.js
+++ script/amazon.js
@@ -1,4 +1,4 @@
-import { cart, add_to_cart} from "../data/cart.js";
+import { cart, add_to_cart } from "../data/cart.js";
 import { product } from "../data/products.js"
 import { format_currency } from "./utils/money.js";
 let prodhtml = '';
@@ -44,7 +44,7 @@ product.forEach((prod) => {
 
           <div class="product-spacer"></div>
 
-          <div class="added-to-cart">
+          <div class="added-to-cart js-added-to-cart-${prod.id}">
             <img src="images/icons/checkmark.png">
             Added
           </div>
@@ -59,21 +59,42 @@ product.forEach((prod) => {
 document.querySelector('.js-products-grid').innerHTML = prodhtml;
 
 //function for the updating total cart quantity in the front page
-function update_cart(){
+function update_cart() {
   let total_quantity = 0;
 
-    cart.forEach((item) => {
-      total_quantity += item.quantity;
-    });
-    document.querySelector('.js-cart-quantity').innerHTML = total_quantity;
+  cart.forEach((item) => {
+    total_quantity += item.quantity;
+  });
+  document.querySelector('.js-cart-quantity').innerHTML = total_quantity;
 }
+
+let time_id;
 
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
   button.addEventListener('click', () => {
     const pro_id = button.dataset.product_id;
     const quantity_select = document.querySelector(`.js-quantity-selector-${pro_id}`);
     const val = Number(quantity_select.value);
-    add_to_cart(pro_id,val);
+    add_to_cart(pro_id, val);
     update_cart();
+    generateAddedhtml(pro_id);
   });
 });
+
+
+// for  'added' to be visible  on the page after clicking adda to cart button
+
+function generateAddedhtml(pro_id) {
+  const added_to = document.querySelector(`.js-added-to-cart-${pro_id}`);
+
+  added_to.classList.add('added-to-cart-visible');
+
+  if (time_id) {
+    clearTimeout(time_id);
+  }
+  const timeID = setTimeout(() => {
+    added_to.classList.remove('added-to-cart-visible');
+  }, 2000);
+  time_id = timeID;
+
+}
