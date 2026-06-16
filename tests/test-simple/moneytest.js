80189e60ef5f3f72b3e48ff802a05a7d45232b87
Mahmud <mahmudekhlas70@gmail.com>
2026-06-17 02:13:22 +0530

    not commited

--- tests/test-simple/moneytest.js
+++ tests/test-simple/moneytest.js
@@ -1,4 +1,4 @@
-import { format_currency } from "../script/utils/money.js";
+import { format_currency } from "../../script/utils/money.js";
 
 if(format_currency(20998) === '209.98'){
     console.log('passed');
